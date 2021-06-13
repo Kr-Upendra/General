994d1ea8ec7dd9891be0e6f08ce62b91ab2b2bd4
Kr-Upendra <83096427+Kr-Upendra@users.noreply.github.com>
2021-06-13 12:56:22 +0530

    Update index.js

--- Random Dice Roll/index.js
+++ Random Dice Roll/index.js
@@ -1,31 +1,5 @@
-// var randomNumber1 = Math.floor(Math.random() * 6) + 1; 
-// // console.log(randomNumber1);
+// A simple Roll Dice Project
 
-// var randomDice = "dice" + randomNumber1 + ".jpeg";
-// // console.log(randomDice);
-
-// var randomDiceSource = "image/" + randomDice;
-// // console.log(randomDiceSource);
-
-// // let image = document.querySelectorAll("img")[1];
-// // // console.log(image);
-// // image.setAttribute("src", randomDiceSource);
-// document.querySelector("button").addEventListener("click", myFunction);
-
-// function myFunction() {
-//     document.querySelectorAll("img")[1].setAttribute("src", randomDiceSource);
-//     // image.setAttribute("src", randomDiceSource);
-// }
-
-// const randomNumber1 = Math.floor(Math.random() * 6) +1;
-// const randomDice = "dice" + randomNumber1 + ".jpeg";
-// const randomDiceSource = "image/" + randomDice;
-// const btn1 = document.querySelector(".btn");
-// const diceImage = document.querySelectorAll("img")[1];
- 
-// btn1.addEventListener("click", function() {
-//     diceImage.setAttribute("img", randomDiceSource);
-// })
 const hideDice = document.querySelector(".diceImage");
 hideDice.classList.add("hidden");
 const btnRoll = document.querySelector(".btn");
