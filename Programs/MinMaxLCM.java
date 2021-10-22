6afe91bcf2f83652264aa4014652f3b57d264b05
Upendra Kumar <83096427+Kr-Upendra@users.noreply.github.com>
2021-10-22 22:27:10 +0530

    Update MinMaxLCM.java

--- Programs/MinMaxLCM.java
+++ Programs/MinMaxLCM.java
@@ -4,17 +4,35 @@ class Codechef {
    public static void main(String[] args) {
       Scanner sc = new Scanner (System.in);
       int t = sc.nextInt();
-      while (t--) {
+      while (t-->0) {
          int x = sc.nextInt();
          int k = sc.nextInt();
-         
-
+//         For min LCM
          System.out.print(2*x);
-         System.out.println();
-         
+         System.out.print(" ");
+//        For max LCM
          System.out.print(x*k*(x*k-1));
          System.out.println();
          
       }
    }
 }
+
+
+
+
+
+
+
+
+         
+
+
+
+         
+
+
+         
+      
+
+
